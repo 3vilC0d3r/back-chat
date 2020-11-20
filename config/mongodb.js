c1076d4b0d9dd85e97ac8454e2c272784f5ff1c9
euclides <euclides.nascimento@vivitech.com>
2020-11-20 13:13:44 -0300

    fixing db connect

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true,  useUnifiedTopology: true })
+mongoose.connect(
+    process.env.MONGO_DB,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .catch(e => {
-        const msg = 'Não foi possível conectar com banco de dados!';
+        const msg = e;
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
     })
